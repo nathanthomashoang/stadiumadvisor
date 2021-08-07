0a0c7acbae82d38547e91f3418d3a2f33506a7c5
NateH <namthomashoang@gmail.com>
2021-08-06 23:09:39 -0700

    first deploy

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Connected to Server - Serving on port: ${port}`);
 })
